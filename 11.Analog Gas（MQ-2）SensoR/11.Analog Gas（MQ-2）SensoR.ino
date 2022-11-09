76127817c4e9ff8d7a3e50ca971954f9107ad3c1
Christian C.C. Emeka <54124037+sonofnos@users.noreply.github.com>
2022-11-09 19:52:41 +0100

    Create 11.Analog Gas（MQ-2）SensoR.ino

---
+++ 11.Analog Gas（MQ-2）SensoR/11.Analog Gas（MQ-2）SensoR.ino
@@ -0,0 +1,26 @@
+int MQ2 = A0; // Define MQ2 gas sensor pin at A0
+int val = 0; // 
+
+
+
+int buzzer = 3; // Define the buzzer pin at D3
+void setup ()
+{
+pinMode (MQ2, INPUT); // MQ2 gas sensor as input
+Serial.begin (9600); // Set the serial port baud rate to 9600
+pinMode (buzzer, OUTPUT); // Set the digital IO pin mode for output
+}
+void loop ()
+{
+val = analogRead (MQ2); // Read the voltage value of A0 port and assign it to val
+Serial.println (val); // Serial port sends val value
+if (val> 450)
+{
+tone (buzzer, 589);
+delay(300);
+}
+else
+{
+noTone (buzzer);
+}
+}
