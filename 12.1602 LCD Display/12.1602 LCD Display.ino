1d3b3f05d3fd9f7d55c07d3366ff5f1daca19107
Christian C.C. Emeka <54124037+sonofnos@users.noreply.github.com>
2022-11-10 13:59:58 +0100

    Create 12.1602 LCD Display.ino

---
+++ 12.1602 LCD Display/12.1602 LCD Display.ino
@@ -0,0 +1,19 @@
+
+#include <Wire.h>
+
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd (0x27,16,2); // set the LCD address to 0x27 for a16 chars and 2 line display
+void setup ()
+{
+lcd.init (); // initialize the lcd
+lcd.init (); // Print a message to the LCD.
+lcd.backlight ();
+lcd.setCursor (3,0);
+lcd.print ("Hello, world!"); // LED print hello, world!
+lcd.setCursor (2,1);
+lcd.print ("Christian!"); // LED print keyestudio!
+}
+void loop ()
+{
+}
